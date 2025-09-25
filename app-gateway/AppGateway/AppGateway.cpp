f37bae1ce7dc74cbcdd02e80dfc8d6dcfea00d3b
Kavia AI <production@kavia.ai>
2025-09-25 07:33:55 +0000

    CheckPoint - cmf8e7ba8d

--- app-gateway/AppGateway/AppGateway.cpp
+++ app-gateway/AppGateway/AppGateway.cpp
@@ -25,7 +25,7 @@ AppGateway::AppGateway()
     , _permissionEnforcement(true)
     , _jwtEnabled(false)
     , _callsign("org.rdk.AppGateway") {
-    std::printf("Hello from AppGateway constructor3!\n");
+    std::printf("Hello from AppGateway constructor4!\n");
     RegisterMethods();
 }
 
