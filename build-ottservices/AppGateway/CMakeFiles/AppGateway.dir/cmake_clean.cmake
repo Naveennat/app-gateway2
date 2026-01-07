2fa7b5af1323ed98a563ded5e23db7fb35b34a06
Kavia AI <production@kavia.ai>
2026-01-07 06:56:42 +0000

    Checkpoint 17 - cmdd9571b1

---
+++ build-ottservices/AppGateway/CMakeFiles/AppGateway.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AppGateway.dir/AppGatewayImplementation.cpp.o"
+  "CMakeFiles/AppGateway.dir/AppGatewayImplementation.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AppGateway.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
