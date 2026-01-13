165109028464706f86f61ce7e64e2c644df0d800
Kavia AI <production@kavia.ai>
2026-01-13 05:59:21 +0000

    Checkpoint 5 - cm130bb936

---
+++ plugin/AppGateway/UtilsConnections.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Forwarding shim:
+// Some upstream code includes "UtilsConnections.h" from the plugin include path.
+// In this repo, the implementation lives under helpers/rdkservices-comcast/helpers.
+#include "../../helpers/rdkservices-comcast/helpers/UtilsConnections.h"
---
+++ plugin/AppGateway/UtilsJsonrpcDirectLink.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Forwarding shim:
+// Some upstream code includes "UtilsJsonrpcDirectLink.h" from the plugin include path.
+// In this repo, the implementation lives under helpers/rdkservices-comcast/helpers.
+#include "../../helpers/rdkservices-comcast/helpers/UtilsJsonrpcDirectLink.h"
